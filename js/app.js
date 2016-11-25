afa7d23da60f025eeca660dc95b264539a672bad
Nan Copeland <ncopeland@industrydive.com>
2016-11-25 15:59:56 -0500

    menu is working, needs styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 // JS
 
 $(document).ready(function(){
+	$('.menu-more-info').hide(); 
 	$('.menu-icon-wrapper').click(function(){
 		$(this).toggleClass('open');
+		$('.menu-more-info').fadeToggle();
 	});
 });
