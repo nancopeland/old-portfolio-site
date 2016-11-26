7bb284fce6610a2bef9e796747c2d23b9b4d58bb
Nan Copeland <ncopeland@industrydive.com>
2016-11-26 16:45:03 -0500

    updates a bit of menu

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 // JS
 
 $(document).ready(function(){
-	$('.menu-more-info').hide(); 
+	$('.menu-about').hide(); 
 	$('.menu-icon-wrapper').click(function(){ // click on "more info" button
 		// opens menu
 		$('.menu').toggleClass('open'); // gives whole menu the "open" class
-		$('.menu-more-info').fadeToggle(); // show "more info" menu
+		$('.menu-about').fadeToggle(); // show "more info" menu
 
 		// replaces "more info" with "close"
 		if ($('.menu').hasClass('open')) {
 			$('.menu-cta').html('<span>close</span>'); 
 		} else {
-			$('.menu-cta').html('<span>more info</span>'); 
+			$('.menu-cta').html('<span>about</span>'); 
 		}
 	});
+
 });
